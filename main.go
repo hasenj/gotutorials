81290979f4910640c83de1ba6ac514419da66562
Hasen Judy <hasen.judy@gmail.com>
2020-07-21 01:50:03 +0900

    Process referenced types

--- main.go
+++ main.go
@@ -5,6 +5,18 @@ import "io"
 import "fmt"
 import "reflect"
 
+type TypeLinker struct {
+	Structs []StructInfo
+	Queue []reflect.Type
+	SeenTypes map[reflect.Type]bool
+}
+
+func NewTypeLinker() *TypeLinker {
+	var linker = new(TypeLinker)
+	linker.SeenTypes = make(map[reflect.Type]bool)
+	return linker
+}
+
 type StructInfo struct {
 	Type reflect.Type
 	Fields []StructFieldInfo
@@ -20,25 +32,27 @@ func TypescriptTypeName(t reflect.Type) string {
 	return t.Name()
 }
 
-func GenerateTypeInfo(inst interface{}) *StructInfo {
-	var result = new(StructInfo)
-	result.Type = reflect.TypeOf(inst)
-	numFields := result.Type.NumField()
+func ProcessType(linker *TypeLinker, t reflect.Type) {
+	var sinfo StructInfo
+	sinfo.Type = t
+	numFields := sinfo.Type.NumField()
 	for index := 0; index < numFields; index++ {
-		field := result.Type.Field(index)
+		field := sinfo.Type.Field(index)
 		// fmt.Printf("%#v\n", field)
-		sfield := StructFieldInfo {
-			Name: field.Name,
-			Type: field.Type,
-			CustomType: field.Tag.Get("ts"),
-		}
+		var sfield StructFieldInfo
+		sfield.Name = field.Name
+		sfield.Type = field.Type
+		sfield.CustomType = field.Tag.Get("ts")
 
-		result.Fields = append(result.Fields, sfield)
+		if sfield.CustomType == "" && sfield.Type.Kind() == reflect.Struct {
+			QueueType(linker, sfield.Type)
+		}
+		sinfo.Fields = append(sinfo.Fields, sfield)
 	}
-	return result
+	linker.Structs = append(linker.Structs, sinfo)
 }
 
-func DescribeStruct(w io.Writer, s *StructInfo) {
+func DescribeStruct(w io.Writer, s StructInfo) {
 	fmt.Fprintf(w, "interface %s { \n", TypescriptTypeName(s.Type))
 	for _, field := range s.Fields {
 		var tstype = field.CustomType
@@ -50,8 +64,37 @@ func DescribeStruct(w io.Writer, s *StructInfo) {
 	fmt.Fprintf(w, "}")
 }
 
+func DescribeTypes(w io.Writer, linker *TypeLinker) {
+	for _, sinfo := range linker.Structs {
+		DescribeStruct(w, sinfo)
+		fmt.Fprintln(w, "\n")
+	}
+}
+
+func QueueInstance(linker *TypeLinker, inst interface{}) {
+	QueueType(linker, reflect.TypeOf(inst))
+}
+
+func QueueType(linker *TypeLinker, t reflect.Type) {
+	if linker.SeenTypes[t] {
+		return
+	}
+	linker.SeenTypes[t] = true
+	linker.Queue = append(linker.Queue, t)
+}
+
+func Process(linker *TypeLinker) {
+	for len(linker.Queue) > 0 {
+		t := linker.Queue[0]
+		linker.Queue = linker.Queue[1:]
+		ProcessType(linker, t)
+	}
+}
+
 func main() {
+	var linker = NewTypeLinker()
 	var inst UserProfile
-	var typeInfo = GenerateTypeInfo(inst)
-	DescribeStruct(os.Stdout, typeInfo)
+	QueueInstance(linker, inst)
+	Process(linker)
+	DescribeTypes(os.Stdout, linker)
 }
