593cbd99359e664816b036f4593e5f54da94ae1e
Hasen Judy <hasen.judy@gmail.com>
2020-07-27 20:30:17 +0900

    move example types to main file

--- main.go
+++ main.go
@@ -2,6 +2,28 @@ package main
 
 import "os"
 
+import "time"
+
+// Some example types to test the go-ts type bridge
+
+type UserLoginInfo struct {
+	UserID   int
+	Name     string
+	IsActive bool
+}
+
+type UserBasicInfo struct {
+	Email string
+	Bio   string
+}
+
+type UserProfile struct {
+	UserLoginInfo
+	Basic     UserBasicInfo
+	LastLogin time.Time `ts:"string"`
+	Friends   	 []UserLoginInfo
+}
+
 func main() {
 	var bridge = NewTypeBridge()
 	var inst UserProfile
